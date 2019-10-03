353d4134b09621dd2c2aaa26ed451bbfd9e57ac8
Michael Mican <michael.mican@stud.th-deg.de>
2019-10-03 16:21:51 +0200

    add build folder

---
+++ build/precache-manifest.adbe83441326a9c22b91af0011552606.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54bf446a4b2933dcd4f065b294d194e1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b70735b0ce0b19ff01bf",
+    "url": "/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "c958aac908bb78641db0",
+    "url": "/static/css/main.2ea24462.chunk.css"
+  },
+  {
+    "revision": "b70735b0ce0b19ff01bf",
+    "url": "/static/js/2.a6ecbf6b.chunk.js"
+  },
+  {
+    "revision": "c958aac908bb78641db0",
+    "url": "/static/js/main.261221f9.chunk.js"
+  },
+  {
+    "revision": "02315fbbfc9f4b03e8ca",
+    "url": "/static/js/runtime-main.8f92a623.js"
+  },
+  {
+    "revision": "7c649a6f2882be25d2093e18d1ca33cb",
+    "url": "/static/media/refresh.7c649a6f.png"
+  }
+]);
